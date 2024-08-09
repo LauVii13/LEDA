37fd66da968d803bd493ea0310b14141918537e2
LauVii <viniciusglaureano@gmail.com>
2024-08-09 18:13:25 -0300

    finished

--- _refatorado/_own_implementations/binarySearch/src/main/java/binarysearch/Main.java
+++ _refatorado/_own_implementations/binarySearch/src/main/java/binarysearch/Main.java
@@ -1,6 +1,7 @@
 package binarysearch;
 
 import binarysearch.implementations.Classic;
+import binarysearch.implementations.Recursive;
 
 public class Main {
   public static void main(String[] args) {
@@ -8,8 +9,10 @@ public class Main {
     Integer[] sortedDistinctNumbers = { 0, 1, 2, 3, 5, 7, 8, 9, 13, 14, 15, 16, 17 };
     Integer[] sortedNumbers = { 0, 0, 1, 1, 1, 2, 3, 5, 5, 5, 7, 8, 9, 9, 13, 13, 14, 14, 14, 15, 16, 16, 16 };
 
-    AbstractBinarySearch<Integer> binarySearch = new Classic<>();
+    // AbstractBinarySearch<Integer> binarySearch = new Classic<>();
+    AbstractBinarySearch<Integer> binarySearch = new Recursive<>();
     System.out.println(binarySearch.binSearch(unitSize, 2));
+    System.out.println(binarySearch.binSearch(unitSize, -2));
     System.out.println(binarySearch.binSearch(sortedDistinctNumbers, -2));
     System.out.println(binarySearch.binSearch(sortedNumbers, 0, 7, 2));
 
