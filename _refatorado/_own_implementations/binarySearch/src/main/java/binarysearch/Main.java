92b6a529a5f241a13a72f9e2a7637752035a2189
LauVii <viniciusglaureano@gmail.com>
2024-08-13 20:42:46 -0300

    finished recursive floor

--- _refatorado/_own_implementations/binarySearch/src/main/java/binarysearch/Main.java
+++ _refatorado/_own_implementations/binarySearch/src/main/java/binarysearch/Main.java
@@ -3,6 +3,7 @@ package binarysearch;
 import binarysearch.implementations.Classic;
 import binarysearch.implementations.Recursive;
 import binarysearch.implementations.RecursiveCeil;
+import binarysearch.implementations.RecursiveFloor;
 
 public class Main {
   public static void main(String[] args) {
@@ -12,11 +13,12 @@ public class Main {
 
     // AbstractBinarySearch<Integer> binarySearch = new Classic<>();
     // AbstractBinarySearch<Integer> binarySearch = new Recursive<>();
-    AbstractBinarySearch<Integer> binarySearch = new RecursiveCeil<>();
+    // AbstractBinarySearch<Integer> binarySearch = new RecursiveCeil<>();
+    // AbstractBinarySearch<Integer> binarySearch = new RecursiveFloor<>();
     System.out.println(binarySearch.binSearch(unitSize, -2));
-    System.out.println(binarySearch.binSearch(unitSize, -2));
-    System.out.println(binarySearch.binSearch(sortedDistinctNumbers, 4));
-    System.out.println(binarySearch.binSearch(sortedNumbers, 100));
+    System.out.println(binarySearch.binSearch(unitSize, 10));
+    System.out.println(binarySearch.binSearch(sortedDistinctNumbers, 18));
+    System.out.println(binarySearch.binSearch(sortedNumbers, 6));
 
   }
 }
