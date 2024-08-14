6c64075fd73eeda91493e2e583f479efb97107f1
LauVii <viniciusglaureano@gmail.com>
2024-08-13 22:06:12 -0300

    finished

--- _refatorado/_own_implementations/binarySearch/src/main/java/binarysearch/Main.java
+++ _refatorado/_own_implementations/binarySearch/src/main/java/binarysearch/Main.java
@@ -4,6 +4,7 @@ import binarysearch.implementations.Classic;
 import binarysearch.implementations.Recursive;
 import binarysearch.implementations.RecursiveCeil;
 import binarysearch.implementations.RecursiveFloor;
+import binarysearch.implementations.RecursiveLastOccurrence;
 import binarysearch.implementations.RecursiveClosest;
 import binarysearch.implementations.RecursiveFirstOccurrence;
 
@@ -18,12 +19,14 @@ public class Main {
     // AbstractBinarySearch<Integer> binarySearch = new RecursiveCeil<>();
     // AbstractBinarySearch<Integer> binarySearch = new RecursiveFloor<>();
     // AbstractBinarySearch<Integer> binarySearch = new RecursiveClosest();
-    AbstractBinarySearch<Integer> binarySearch = new RecursiveFirstOccurrence<>();
+    // AbstractBinarySearch<Integer> binarySearch = new
+    // RecursiveFirstOccurrence<>();
+    AbstractBinarySearch<Integer> binarySearch = new RecursiveLastOccurrence<>();
 
     System.out.println(binarySearch.binSearch(unitSize, -2));
     System.out.println(binarySearch.binSearch(unitSize, 10));
     System.out.println(binarySearch.binSearch(sortedDistinctNumbers, 2));
-    System.out.println(binarySearch.binSearch(sortedNumbers, 1));
+    System.out.println(binarySearch.binSearch(sortedNumbers, 0));
 
   }
 }
