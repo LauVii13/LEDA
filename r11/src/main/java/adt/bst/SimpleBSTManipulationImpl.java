33a91d2d71fc17eb36cd5e931411806a11cc6a2a
LauVii <viniciusglaureano@gmail.com>
2024-09-16 21:14:43 -0300

    finished

--- r11/src/main/java/adt/bst/SimpleBSTManipulationImpl.java
+++ r11/src/main/java/adt/bst/SimpleBSTManipulationImpl.java
@@ -1,29 +1,84 @@
 package adt.bst;
 
 /**
- * - Esta eh a unica classe que pode ser modificada 
+ * - Esta eh a unica classe que pode ser modificada
+ * 
  * @author adalbertocajueiro
  *
  * @param <T>
  */
 public class SimpleBSTManipulationImpl<T extends Comparable<T>> implements SimpleBSTManipulation<T> {
 
-	@Override
-	public boolean equals(BST<T> tree1, BST<T> tree2) {
-		// TODO Implement this method
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+  @Override
+  public boolean equals(BST<T> tree1, BST<T> tree2) {
+    return equals((BSTNode<T>) tree1.getRoot(), (BSTNode<T>) tree2.getRoot());
+  }
 
-	@Override
-	public boolean isSimilar(BST<T> tree1, BST<T> tree2) {
-		// TODO Implement this method
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+  private boolean equals(BSTNode<T> node1, BSTNode<T> node2) {
+    boolean result = false;
 
-	@Override
-	public T orderStatistic(BST<T> tree, int k) {
-		// TODO Implement this method
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+    if (areBothEmpty(node1, node2)) {
+      result = true;
+    } else if (areEquals(node1, node2)) {
+      result = equals((BSTNode<T>) node1.getLeft(), (BSTNode<T>) node2.getLeft())
+          && equals((BSTNode<T>) node1.getRight(), (BSTNode<T>) node2.getRight());
+    }
 
+    return result;
+  }
+
+  @Override
+  public boolean isSimilar(BST<T> tree1, BST<T> tree2) {
+    return isSimilar((BSTNode<T>) tree1.getRoot(), (BSTNode<T>) tree2.getRoot());
+  }
+
+  private boolean isSimilar(BSTNode<T> node1, BSTNode<T> node2) {
+    boolean result = false;
+
+    if (areBothEmpty(node1, node2)) {
+      result = true;
+    } else if (bothHaveData(node1, node2)) {
+      result = isSimilar((BSTNode<T>) node1.getLeft(), (BSTNode<T>) node2.getLeft())
+          && isSimilar((BSTNode<T>) node1.getRight(), (BSTNode<T>) node2.getRight());
+    }
+
+    return result;
+  }
+
+  @Override
+  public T orderStatistic(BST<T> tree, int k) {
+    T result = null;
+
+    if (k >= 1) {
+      result = orderStatistic(tree, tree.minimum(), k);
+    }
+
+    return result;
+  }
+
+  private T orderStatistic(BST<T> tree, BSTNode<T> node, int k) {
+    T result = null;
+
+    if (!node.isEmpty()) {
+      if (k == 1) {
+        result = node.getData();
+      } else {
+        result = orderStatistic(tree, tree.sucessor(node.getData()), --k);
+      }
+    }
+
+    return result;
+  }
+
+  private boolean areBothEmpty(BSTNode<T> node1, BSTNode<T> node2) {
+    return node1.isEmpty() && node2.isEmpty();
+  }
+
+  private boolean areEquals(BSTNode<T> node1, BSTNode<T> node2) {
+    return bothHaveData(node1, node2) && node1.getData().compareTo(node2.getData()) == 0;
+  }
+
+  private boolean bothHaveData(BSTNode<T> node1, BSTNode<T> node2) {
+    return !node1.isEmpty() && !node2.isEmpty();
+  }
 }
